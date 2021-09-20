639c966db61c7cec8f6a76debde1236160384571
nhatbao2812 <tran.nhat.bao281295@gmail.com>
2021-09-20 23:11:07 +0700

    Update DemoController.java

--- src/main/java/com/example/demo/Controller/DemoController.java
+++ src/main/java/com/example/demo/Controller/DemoController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/")
 public class DemoController {
 
-    private HelloService helloService;
+    private final HelloService helloService;
 
     @Autowired
     public DemoController(HelloService helloService) {
