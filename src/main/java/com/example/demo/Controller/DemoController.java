d2416273c66c2f3e231eb4b7d497659c32b8a557
nhatbao2812 <tran.nhat.bao281295@gmail.com>
2021-09-20 23:00:55 +0700

    Update DemoController.java

--- src/main/java/com/example/demo/Controller/DemoController.java
+++ src/main/java/com/example/demo/Controller/DemoController.java
@@ -2,9 +2,9 @@ package com.example.demo.Controller;
 
 import com.example.demo.Service.HelloService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,12 +18,12 @@ public class DemoController {
         this.helloService = helloService;
     }
 
-    @RequestMapping(method = RequestMethod.GET, value = "/hello")
+    @GetMapping(value = "/hello")
     public String hello() {
         return "hello";
     }
 
-    @RequestMapping(method = RequestMethod.GET, value = "/hello/{value}")
+    @GetMapping(value = "/hello/{value}")
     public String helloFromAnotherSide(@PathVariable("value") String value) {
         String finalValue = helloService.handleHello(value);
         return "hello " + finalValue;
